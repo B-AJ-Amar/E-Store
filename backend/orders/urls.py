6b3a8fa84f172e6ac2121709af934af3dad2d7dc
Bkz-Amar <amarbouakaz91@gmail.com>
2023-07-26 02:49:40 +0100

    put get orders/details/id

--- backend/orders/urls.py
+++ backend/orders/urls.py
@@ -4,6 +4,7 @@ from .views import *
 urlpatterns = [
 #    path('', OrderProdView.as_view()),
     path('', OrderView.as_view()),
+    path('details/<int:id>', OrderDetailsView.as_view()),
     
 
 ]
